104756eff975f9cf6945bf3cfed7b84a4fdb5395
Hao Liu <139726000+LucasLiu3@users.noreply.github.com>
2024-11-28 17:29:52 +1300

    update

--- LevelManangementSystem.web/Data/ApplicationDbContext.cs
+++ LevelManangementSystem.web/Data/ApplicationDbContext.cs
@@ -9,5 +9,10 @@ namespace LevelManangementSystem.web.Data
             : base(options)
         {
         }
+
+        //现在Data文件夹下创建要用的table，里面创建字段， 然后在这里告诉数据库，连接想要的表，然后给表起的名
+        //然后在package manager console里面进行migration，输入 add-migration 表名
+        //然后会在Migrations的文件夹下创建你的table，最后在console 输入update-database
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
---
+++ LevelManangementSystem.web/Data/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿namespace LevelManangementSystem.web.Data
+{
+    public class LeaveType
+    {
+      public int Id {  get; set; }
+
+      public string Name { get; set; }
+      
+      public int NumberOfDays {  get; set; }
+
+
+    }
+}
